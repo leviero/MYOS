1f66b31e79cd9d14cfd7f2e592a6a015c5fa698d
Leviero <leviero@gmail.com>
2015-03-28 13:46:37 +0800

    login function updated

--- app/src/main/java/com/myos/myos/LogIn.java
+++ app/src/main/java/com/myos/myos/LogIn.java
@@ -1,17 +1,30 @@
 package com.myos.myos;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
-public class LogIn extends Activity {
+public class LogIn extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+
+        Button signupButton = (Button) findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(this);
+
     }
 
 
@@ -35,5 +48,25 @@ public class LogIn extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+
+        Intent mIntent;
+
+        switch (v.getId()){
+            case R.id.loginButton:
+                mIntent = new Intent(this, Salon.class);
+                mIntent.putExtra("username", username);
+                break;
+            case R.id.signupButton:
+                mIntent = new Intent(this, SignUp.class);
+                break;
+            default:
+                break;
+        }
     }
 }
